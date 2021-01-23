dbaad57f94cb8736e19b5e552b63267ee5b8e501
Jeremy <jeremyloyingping@googlemail.com>
2021-01-23 17:55:26 +0000

    Use numpy.
    I've rewritten the agent to use numpy for the position and velocity vectors.

--- epidemisim/simulation/agent.py
+++ epidemisim/simulation/agent.py
@@ -1,43 +1,24 @@
-
-
-class Position:
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return "(x: {0}, y: {0})".format(self.x, self.y)
-
-
-class Velocity:
-
-    def __init__(self, dx, dy):
-        self.dx = dx
-        self.dy = dy
-
-    def __str__(self):
-        return "(dx: {0}, dy: {0})".format(self.x, self.y)
+import numpy as np
 
 
 class Agent:
 
-    def __init__(self, position: Position, velocity: Velocity):
+    def __init__(self, position: np.ndarray, velocity: np.ndarray):
         self.position = position
         self.velocity = velocity
 
     def update(self):
-        pass
+        self.position += self.velocity
 
 
 if __name__ == '__main__':
 
     import time
 
-    a = Agent(Position(0, 0), Velocity(2, 1))
-    #b = Agent(Position(0, 0), Velocity(2, 1))
+    a = Agent(np.array([0, 0]), np.array([2, 1]))
+    b = Agent(np.array([5, -3]), np.array([-1, 1]))
 
-    agents = [a]
+    agents = [a, b]
 
     while True:
         i = 0
